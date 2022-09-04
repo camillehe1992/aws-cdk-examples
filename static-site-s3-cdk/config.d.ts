c2a0b520de0a377a5a8787635a094103f4c4b6cf
He.Camille <camille.he@outlook.com>
2022-09-04 18:30:16 +0800

    chore: remove generated files

--- static-site-s3-cdk/config.d.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-declare const _default: {
-    account: string | undefined;
-    region: string | undefined;
-};
-export default _default;
--- static-site-s3-cdk/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-exports.default = {
-    // General
-    account: process.env.AWS_ACCOUNT_ID,
-    region: process.env.AWS_REGION
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY29uZmlnLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEseUJBQXNCO0FBRXRCLGtCQUFlO0lBQ2IsVUFBVTtJQUNWLE9BQU8sRUFBRSxPQUFPLENBQUMsR0FBRyxDQUFDLGNBQWM7SUFDbkMsTUFBTSxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsVUFBVTtDQUMvQixDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0ICdkb3RlbnYvY29uZmlnJ1xyXG5cclxuZXhwb3J0IGRlZmF1bHQge1xyXG4gIC8vIEdlbmVyYWxcclxuICBhY2NvdW50OiBwcm9jZXNzLmVudi5BV1NfQUNDT1VOVF9JRCxcclxuICByZWdpb246IHByb2Nlc3MuZW52LkFXU19SRUdJT05cclxufTsiXX0=
