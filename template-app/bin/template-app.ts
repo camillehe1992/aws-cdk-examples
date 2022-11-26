58027a85c45423b57476e89c415afe7703cc2598
He.Camille <camille.he@outlook.com>
2022-11-26 15:56:36 +0800

    feat: remove config file from template

--- template-app/bin/template-app.ts
+++ template-app/bin/template-app.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
+import 'dotenv/config';
 import * as cdk from 'aws-cdk-lib';
 import { TemplateAppStack } from '../lib/template-app-stack';
 
+const env = {
+  account: process.env.AWS_ACCOUNT_ID,
+  region: process.env.AWS_REGION
+}
+
 const app = new cdk.App();
-new TemplateAppStack(app, 'TemplateAppStack');
+new TemplateAppStack(app, 'TemplateAppStack', { env });
--- template-app/config.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-
-export default {
-  // General
-  account: process.env.AWS_ACCOUNT_ID,
-  region: process.env.AWS_REGION
-  
-};
